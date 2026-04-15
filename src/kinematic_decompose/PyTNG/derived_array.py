16964d5fd118a23743b65d633123e6e91018224f
quadrup1efish <lty13313730458@gmail.com>
2026-04-15 16:32:22 +0800

    ✨ Add star luminosity of derived_array.py

--- src/kinematic_decompose/PyTNG/derived_array.py
+++ src/kinematic_decompose/PyTNG/derived_array.py
@@ -291,3 +291,124 @@ def temp(sim):
 
     T.convert_units('K')
     return T
+
+@pynbody.derived_array
+def U_mag(sim):
+    """
+    Vega magnitudes
+    see https://www.tng-project.org/data/docs/specifications/#parttype4 for details
+    In detail, these are:
+    Buser's X filter, where X=U,B3,V (Vega magnitudes),
+    then IR K filter + Palomar 200 IR detectors + atmosphere.57 (Vega),
+    then SDSS Camera X Response Function, airmass = 1.3 (June 2001), where X=g,r,i,z (AB magnitudes).
+    They can be found in the filters.log file in the BC03 package.
+    The details on the four SDSS filters can be found in Stoughton et al. 2002, section 3.2.1.
+    """
+
+    if 'GFM_StellarPhotometrics' not in sim:
+        print("Need 'GFM_StellarPhotometrics' of star ")
+
+    return sim['GFM_StellarPhotometrics'][:, 0]
+
+@pynbody.derived_array
+def U_lum(sim):
+    """3571  unit Lsun"""
+    return 10.0 ** (-0.4*(sim.s['U_mag']-5.55))
+
+# star
+@pynbody.derived_array
+def B_mag(sim):
+    """Vega magnitudes"""
+    if 'GFM_StellarPhotometrics' not in sim:
+        print("Need 'GFM_StellarPhotometrics' of star ")
+
+    return sim['GFM_StellarPhotometrics'][:, 1]
+
+@pynbody.derived_array
+def B_lum(sim):
+    """4344 unit Lsun"""
+    return 10.0 ** (-0.4*(sim.s['B_mag']-5.45))
+
+# star
+@pynbody.derived_array
+def V_mag(sim):
+    """ Vega magnitudes """
+    if 'GFM_StellarPhotometrics' not in sim:
+        print("Need 'GFM_StellarPhotometrics' of star ")
+
+    return sim['GFM_StellarPhotometrics'][:, 2]
+
+@pynbody.derived_array
+def V_lum(sim):
+    """5456 unit Lsun"""
+    return 10.0 ** (-0.4*(sim.s['V_mag']-4.78))
+
+# star
+@pynbody.derived_array
+def K_mag(sim):
+    """ Vega magnitudes"""
+    if 'GFM_StellarPhotometrics' not in sim:
+        print("Need 'GFM_StellarPhotometrics' of star ")
+
+    return sim['GFM_StellarPhotometrics'][:, 3]
+
+@pynbody.derived_array
+def K_lum(sim):
+    """21603 unit Lsun"""
+    return 10.0 ** (-0.4*(sim.s['K_mag']-3.29))
+
+# star
+@pynbody.derived_array
+def g_mag(sim):
+    """AB magnitudes """
+    if 'GFM_StellarPhotometrics' not in sim:
+        print("Need 'GFM_StellarPhotometrics' of star ")
+
+    return sim['GFM_StellarPhotometrics'][:, 4]
+
+@pynbody.derived_array
+def g_lum(sim):
+    """4670 unit Lsun"""
+    return 10.0 ** (-0.4*(sim.s['g_mag']-5.12))
+
+# star
+@pynbody.derived_array
+def r_mag(sim):
+    """AB magnitudes """
+    if 'GFM_StellarPhotometrics' not in sim:
+        print("Need 'GFM_StellarPhotometrics' of star ")
+
+    return sim['GFM_StellarPhotometrics'][:, 5]
+
+@pynbody.derived_array
+def r_lum(sim):
+    """6156 unit Lsun"""
+    return 10.0 ** (-0.4*(sim.s['r_mag']-4.64))
+
+# star
+@pynbody.derived_array
+def i_mag(sim):
+    """AB magnitudes """
+    if 'GFM_StellarPhotometrics' not in sim:
+        print("Need 'GFM_StellarPhotometrics' of star ")
+
+    return sim['GFM_StellarPhotometrics'][:, 6]
+
+@pynbody.derived_array
+def i_lum(sim):
+    """7472 unit Lsun"""
+    return 10.0 ** (-0.4*(sim.s['i_mag']-4.53))
+
+# star
+@pynbody.derived_array
+def z_mag(sim):
+    """AB magnitudes """
+    if 'GFM_StellarPhotometrics' not in sim:
+        print("Need 'GFM_StellarPhotometrics' of star ")
+
+    return sim['GFM_StellarPhotometrics'][:, 7]
+
+@pynbody.derived_array
+def z_lum(sim):
+    """8917 unit Lsun"""
+    return 10.0 ** (-0.4*(sim.s['z_mag']-4.51))
